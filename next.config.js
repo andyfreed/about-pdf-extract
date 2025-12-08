7b25f52c89c0c1821d4730017efd1f4e5c5df6d7
Storrow <44973843+andyfreed@users.noreply.github.com>
2025-12-08 16:05:58 -0500

    Fix: Remove deprecated experimental.serverActions option

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Enable serverless functions for Vercel
-  experimental: {
-    serverActions: true,
-  },
 }
 
 module.exports = nextConfig
